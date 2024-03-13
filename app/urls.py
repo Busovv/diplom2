e6b630f865422fb6fa88f2029ee286cf30f99c5f
Busovv <glsterab@gmail.com>
2024-03-13 21:35:13 +0300

    добаввлена модель products, создана функция products, добавлен товар через админку, добавлен класс меты для products, /media/ добавлен в gitignore, прописана директория для /media/ в settings

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from app.settings import MEDIA_ROOT, MEDIA_URL
 import main.urls as main_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_urls.urlpatterns)),
-]
+    path('products/', include(main_urls.urlpatterns)),
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 
 def home(request: HttpRequest):
     return HttpResponse(render(request, 'home.html', {}))
+
+
+def products(request: HttpRequest):
+    products_list = Product.objects.all()
+    return HttpResponse(render(request, 'products.html', {
+        'products': products_list
+    }))
